22fcd7629f107f5aee230b783ac09197984b8026
swaraj01 <46791587+swaraj01@users.noreply.github.com>
2019-02-08 09:59:32 -0500

    Add files via upload

--- ConsoleApp7/triangletest/triangletest.cs
+++ ConsoleApp7/triangletest/triangletest.cs
@@ -9,22 +9,144 @@ using ConsoleApp7;
 namespace triangletest
 {
     [TestFixture]
-    public class triangletest
+    public  class triangletest
     {
             [Test]
-            public void analyze_inputside1_expectLengthEquals8()
+               public void analyze_inputsides666_expectresultequilateral()
             {
                 //Arrange
-                int s1 = 8;
-                int s2= 6;
+                int s1 = 6;
+                int s2 = 6;
                 int s3 = 6;
-                Trianglesolver triangletest = new Trianglesolver();
-
+             
+            //used these values to check equilateral triagle i.e. all 3 sides are equal
                 //Act
-                int length = triangletest.analyze(s1, s2,s3);
+                string y = "equilateral triangle";
+                string x = Trianglesolver.analyze(s1,s2,s3);
 
                 //Assert
-                Assert.AreEqual(length, l);
-            }
+                
+            Assert.AreEqual(x,y);
         }
+        [Test]
+        public void analyze_inputsides946_expectresultscalenetriangle()
+        {
+            //Arrange
+            int s1 = 9;
+            int s2 = 4;
+            int s3 = 6;
+
+            //used these values to check scalene triangle i.e. all 3 sides are unequal 
+            //Act
+            string y = "scalene triangle";
+            string x = Trianglesolver.analyze(s1, s2, s3);
+
+            //Assert
+
+            Assert.AreEqual(x, y);
+        }
+        [Test]
+        public void analyze_inputsides445_expectresultisoscelestriangle()
+        {
+            //Arrange
+            int s1 = 4;
+            int s2 = 4;
+            int s3 = 5;
+            //used these values to  check isosceles  triangle i.e checking side1 and side2 equal
+
+            //Act
+            string y = "isosceles triangle";
+            string x = Trianglesolver.analyze(s1, s2, s3);
+
+            //Assert
+
+            Assert.AreEqual(x, y);
+        }
+        [Test]
+        public void analyze_inputsides855_expectresultisoscelestriangle()
+        {
+            //Arrange
+            int s1 = 8;
+            int s2 = 5;
+            int s3 = 5;
+            //used these values to  check isosceles  triangle i.e checking side2 and side3 equal
+
+            //Act
+            string y = "isosceles triangle";
+            string x = Trianglesolver.analyze(s1, s2, s3);
+
+            //Assert
+
+            Assert.AreEqual(x, y);
+        }
+        [Test]
+        public void analyze_inputsides757_expectresultisoscelestriangle()
+        {
+            //Arrange
+            int s1 = 7;
+            int s2 = 5;
+            int s3 = 7;
+            //used these values to  check isosceles  triangle i.e checking side1 and side3 equal
+
+            //Act
+            string y = "isosceles triangle";
+            string x = Trianglesolver.analyze(s1, s2, s3);
+
+            //Assert
+
+            Assert.AreEqual(x, y);
+        }
+        [Test]
+        public void analyze_inputsides439_expectresultnotatriangle()
+        {
+            //Arrange
+            int s1 = 4;
+            int s2 = 3;
+            int s3 = 9;
+            //used values as sum of side1 and side2 entered is less than the side3, i.e. not a triangle
+
+
+            //Act
+            string y = "not a triangle";
+            string x = Trianglesolver.analyze(s1, s2, s3);
+
+            //Assert
+
+            Assert.AreEqual(x, y);
+        }
+        [Test]
+        public void analyze_inputsides5113_expectresultnotatriangle()
+        {
+            //Arrange
+            int s1 = 5;
+            int s2 = 11;
+            int s3 = 3;
+            //used values as sum of side1 and side3 entered is less than the side2, i.e. not a triangle
+
+            //Act
+            string y = "not a triangle";
+            string x = Trianglesolver.analyze(s1, s2, s3);
+
+            //Assert
+
+            Assert.AreEqual(x, y);
+        }
+        [Test]
+        public void analyze_inputsides732_expectresultnotatriangle()
+        {
+            //Arrange
+            int s1 = 7;
+            int s2 = 3;
+            int s3 = 2;
+            //used values as sum of side2 and side3 entered is less than the side1, i.e. not a triangle
+
+            //Act
+            string y = "not a triangle";
+            string x = Trianglesolver.analyze(s1, s2, s3);
+
+            //Assert
+
+            Assert.AreEqual(x, y);
+        }
+    }
 }
